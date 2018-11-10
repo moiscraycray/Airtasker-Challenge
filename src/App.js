22316041ac39e30f3849718020bab8e99a86a484
Olivia Mo <oliviatrademark@gmail.com>
2018-11-10 13:10:17 +1100

    Load in data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import data from './data/activity_feed.json';
 
 class App extends Component {
+
+  componentDidMount() {
+    console.log(data)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <h1>test</h1>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <h1>hello</h1>
+      </Fragment>
     );
   }
 }
