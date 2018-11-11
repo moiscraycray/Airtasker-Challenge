e084a87078e70d33fccbdfa5c7576d7e130b73ac
Olivia Mo <oliviatrademark@gmail.com>
2018-11-11 14:18:58 +1100

    Remove App.js and App.css and move ActivityFeed into index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Fragment, Component } from 'react';
 import ReactDOM from 'react-dom';
-import './reflex.css';
-import './index.css';
-import App from './App';
+import './styles/reflex.css';
+import './styles/index.css';
+import data from './data/activity_feed.json';
+import ActivityFeed from './components/activity-feed.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Index extends Component {
+
+  render() {
+    return (
+      <Fragment>
+        <div className="container">
+          <div className="grid box_shadow">
+            <ActivityFeed data={data} />
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
